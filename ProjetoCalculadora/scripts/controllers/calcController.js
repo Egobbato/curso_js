8a1c2876c43e44ce26492e5033d6793abf6be8d7
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-10-25 20:40:32 -0300

    Switch

--- ProjetoCalculadora/scripts/controllers/calcController.js
+++ ProjetoCalculadora/scripts/controllers/calcController.js
@@ -1,5 +1,6 @@
 class CalcController {
   constructor() {
+    this._operation = [];
     this._locale = "pt-BR";
     this._displayEl = document.querySelector("#display");
     this._horaEl = document.querySelector("#hora");
@@ -18,9 +19,64 @@ class CalcController {
   }
 
   addEventListenerAll(element, events, fn) {
-    events.split(" ").forEach((event) => {
-      element.addEventListener(event, fn, false);
+    events.split(" ").forEach((e) => {
+      element.addEventListener(e, fn, false);
     });
+  }
+
+  clearAll() {
+    this._operation = [];
+  }
+
+  clearEntry() {
+    this._operation.pop();
+  }
+
+  addOperation(valor) {
+    this._operation.push(value);
+  }
+
+  setError() {
+    this.displayCalc = "Error";
+  }
+
+  execBtn(valor) {
+    switch (valor) {
+      case "ac":
+        this.clearAll();
+        break;
+      case "ce":
+        this.clearEntry();
+        break;
+      case "soma":
+        break;
+      case "subtracao":
+        break;
+      case "multiplicacao":
+        break;
+      case "divisao":
+        break;
+      case "porcento":
+        break;
+      case "igual":
+        break;
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        this.addOperation(parseInt(value));
+        break;
+
+      default:
+        this.setError();
+        break;
+    }
   }
 
   initButtonsEvents() {
@@ -28,7 +84,10 @@ class CalcController {
 
     buttons.forEach((btn, index) => {
       this.addEventListenerAll(btn, "click drag", (e) => {
-        console.log(btn.className.baseVal.replace("btn-", ""));
+        const texBtn = btn.className.baseVal.replace("btn-", "");
+        console.log(texBtn);
+
+        this.execBtn(texBtn);
       });
 
       this.addEventListenerAll(btn, "mouseover mouseup mousedown", (e) => {
