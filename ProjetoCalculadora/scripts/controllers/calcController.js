da38eb16137318791140d8f49fd9685b7e35888e
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-10-30 21:30:17 -0300

    Correções de Bugs com o Ponto

--- ProjetoCalculadora/scripts/controllers/calcController.js
+++ ProjetoCalculadora/scripts/controllers/calcController.js
@@ -136,7 +136,7 @@ class CalcController {
         this.pushOperation(valor);
       } else {
         const newValue = this.getLastOperation().toString() + valor.toString();
-        this.setLastOperation(parseFloat(newValue));
+        this.setLastOperation(newValue);
 
         this.setLastNumberToDisplay();
       }
@@ -150,6 +150,12 @@ class CalcController {
 
   addDot() {
     let lastOperation = this.getLastOperation();
+
+    if (
+      typeof lastOperation === "string" &&
+      lastOperation.split("").indexOf(".") > -1
+    )
+      return;
 
     if (this.isOperator(lastOperation) || !lastOperation) {
       this.pushOperation("0.");
