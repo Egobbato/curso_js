2ab87b618047441202df53c287c53235c79f1a27
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-11-03 20:40:21 -0300

    Variaveis e Seletores

---
+++ Gerenciamento_Usuario/index.js
@@ -0,0 +1,10 @@
+const nome = document.querySelector("#exampleInputName");
+const genero = document.querySelectorAll(
+  "#form-user-create [name = gender]:checked"
+);
+const nascimento = document.querySelector("#exampleInputBirth");
+const pais = document.querySelector("#exampleInputCountry");
+const eMail = document.querySelector("#exampleInputEmail");
+const senha = document.querySelector("#exampleInputPassword");
+const foto = document.querySelector("#exampleInputFile");
+const admin = document.querySelector("#exampleInputAdmin");
