5e5c657649c81c098fc9df49cc7d1f813ef20358
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-11-10 22:05:54 -0300

    Trabalhando com Funções

--- Gerenciamento_Usuario/index.js
+++ Gerenciamento_Usuario/index.js
@@ -31,5 +31,24 @@ document.getElementById("form-user-create").addEventListener("submit", (e) => {
     }
   });
 
-  console.log(user);
+  addLine(user);
 });
+
+function addLine(dataUser) {
+  const tr = document.createElement("tr");
+
+  tr.innerHTML = `
+  
+  <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+  <td>${dataUser.name}</td>
+  <td>${dataUser.email}</td>
+  <td>${dataUser.admin}</td>
+  <td>${dataUser.birth}</td>
+  <td>
+    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+  </td>
+  `;
+
+  document.getElementById("table-users").appendChild(tr);
+}
