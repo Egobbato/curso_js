dc3c07e9a9d329455983d3d9966d1e539e58a905
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-11-11 23:11:57 -0300

    Programação Orientada a Objetos - Classes e MVC

--- Gerenciamento_Usuario/index.js
+++ Gerenciamento_Usuario/index.js
@@ -31,13 +31,24 @@ document.getElementById("form-user-create").addEventListener("submit", (e) => {
     }
   });
 
-  addLine(user);
+  const objectUser = new User(
+    user.name,
+    user.gender,
+    user.birth,
+    user.country,
+    user.email,
+    user.password,
+    user.photo,
+    user.admin
+  );
+
+  addLine(objectUser);
 });
 
 function addLine(dataUser) {
-  const tr = document.createElement("tr");
+  console.log(dataUser);
 
-  tr.innerHTML = `
+  document.getElementById("table-users").innerHTML = `
   
   <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
   <td>${dataUser.name}</td>
@@ -49,6 +60,4 @@ function addLine(dataUser) {
     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
   </td>
   `;
-
-  document.getElementById("table-users").appendChild(tr);
 }
