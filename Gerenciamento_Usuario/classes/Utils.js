07baa6581e25bb7d0617e8b66afe7cb868727175
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-11-19 20:31:35 -0300

    Trabalhando com Metodos Estaticos

---
+++ Gerenciamento_Usuario/classes/Utils.js
@@ -0,0 +1,15 @@
+class Utils {
+  static dateformate(date) {
+    return (
+      date.getDate() +
+      "/" +
+      (date.getMonth() + 1) +
+      "/" +
+      date.getFullYear() +
+      " " +
+      date.getHours() +
+      ":" +
+      date.getMinutes()
+    );
+  }
+}
