e62c2250fcb7d862c77a0aa92eca36ab862f0c98
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-11-20 18:43:50 -0300

    Validando Formulários

--- Gerenciamento_Usuario/controllers/userController.js
+++ Gerenciamento_Usuario/controllers/userController.js
@@ -63,19 +63,31 @@ class UserController {
 
   getValues() {
     let user = {};
+    let isValid = true;
 
-    [...this.formEl.elements].forEach((campo, index) => {
-      if (campo.name == "gender") {
-        if (campo.checked) {
-          user[campo.name] = campo.value;
-        } else if (campo.name == "admin") {
-          user[campo.name] = campo.checked;
-        }
-      } else {
-        user[campo.name] = campo.value;
+    [...this.formEl.elements].forEach(function (field, index) {
+      if (
+        ["name", "email", "password"].indexOf(field.name) > -1 &&
+        !field.value
+      ) {
+        field.parentElement.classList.add("has-error");
+        isValid = false;
       }
-      console.log(this.formEl);
+
+      if (field.name === "gender") {
+        if (field.checked) {
+          user[field.name] = field.value;
+        }
+      } else if (field.name == "admin") {
+        user[field.name] = field.checked;
+      } else {
+        user[field.name] = field.value;
+      }
     });
+
+    if (!isValid) {
+      return false;
+    }
 
     return new User(
       user.name,
