a836c7b9d5a02d72215ab86167bd83cb30d1fdeb
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-12-01 19:33:29 -0300

    Detectando URL diferente

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -2,9 +2,31 @@ const http = require("http");
 
 let server = http.createServer((req, res) => {
   console.log("URL:", req.url);
-  console.log("METHOD:", res.method);
+  console.log("METHOD:", req.method);
 
-  res.end("OK");
+  switch (req.url) {
+    case "/":
+      res.statusCode = 200;
+      res.setHeader("Content-type", "text/html");
+      res.end("<h1>Olá Mundo</h1>");
+      break;
+
+    case "/users":
+      res.statusCode = 200;
+      res.setHeader("Content-type", "application/json");
+      res.end(
+        JSON.stringify({
+          users: [
+            {
+              name: "Gobbato",
+              email: "egobbato@teste.com.br",
+              id: 1,
+            },
+          ],
+        })
+      );
+      break;
+  }
 });
 
 server.listen(4000, "127.0.0.1", () => {
