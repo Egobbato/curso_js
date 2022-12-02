8f66d8d4c83b7158042fae2787d2e88895393480
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-12-01 21:29:28 -0300

    Carregando Rotas com Consign

--- NodeJs/Routes/users.js
+++ NodeJs/Routes/users.js
@@ -1,32 +1,29 @@
-let express = require("express");
-let routes = express.Router();
-
-routes.get("/", (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "application/json");
-  res.json({
-    users: [
-      {
-        name: "Gobbato",
-        email: "egobbato@teste.com",
-        id: 1,
-      },
-    ],
+module.exports = (app) => {
+  app.get("/users", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-type", "application/json");
+    res.json({
+      users: [
+        {
+          name: "Gobbato",
+          email: "egobbato@teste.com",
+          id: 1,
+        },
+      ],
+    });
   });
-});
 
-routes.get("/admin", (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "application/json");
-  res.json({
-    users: [
-      {
-        administrador: "Eduardo",
-        email: "egobbato@teste.com",
-        id: 1,
-      },
-    ],
+  app.get("/users/admin", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-type", "application/json");
+    res.json({
+      users: [
+        {
+          administrador: "Eduardo",
+          email: "egobbato@teste.com",
+          id: 1,
+        },
+      ],
+    });
   });
-});
-
-module.exports = routes;
+};
--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const routesIndex = require("./Routes/index.js");
-const routesUsers = require("./Routes/users");
+const consign = require("consign");
 
 const app = express();
 
-app.use(routesIndex);
-app.use("/users", routesUsers);
+consign().include("Routes").into(app);
 
 app.listen(4000, () => {
   console.log("Servido Rodando!");
