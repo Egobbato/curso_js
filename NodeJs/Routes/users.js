4ee341f86d8d973354045b45eea7330a785cc48c
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-12-02 22:33:39 -0300

    Recebendo dados via POST e usando o Insomnia

--- NodeJs/Routes/users.js
+++ NodeJs/Routes/users.js
@@ -13,17 +13,7 @@ module.exports = (app) => {
     });
   });
 
-  app.get("/users/admin", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-type", "application/json");
-    res.json({
-      users: [
-        {
-          administrador: "Eduardo",
-          email: "egobbato@teste.com",
-          id: 1,
-        },
-      ],
-    });
+  app.post("/users/admin", (req, res) => {
+    res.json(req.body);
   });
 };
