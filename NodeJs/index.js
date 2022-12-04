0f9c0633f13bac52b3df844239e52b824b6c8fb4
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-12-03 21:16:38 -0300

    Refatorando Sistema e Otimizando o código

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-consign().include("Routes").into(app);
+consign().include("Routes").include("utils").into(app);
 
 app.listen(4000, () => {
   console.log("Servido Rodando!");
