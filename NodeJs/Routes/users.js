1f1be546a244b096bf8e1d8354b3b3a5b83a7d91
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-12-03 21:00:27 -0300

    Listando os usuários do banco NeDB

--- NodeJs/Routes/users.js
+++ NodeJs/Routes/users.js
@@ -6,17 +6,22 @@ const db = new nedb({
 
 module.exports = (app) => {
   app.get("/users", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-type", "application/json");
-    res.json({
-      users: [
-        {
-          name: "Gobbato",
-          email: "egobbato@teste.com",
-          id: 1,
-        },
-      ],
-    });
+    db.find({})
+      .sort({ name: 1 })
+      .exec((err, users) => {
+        if (err) {
+          console.log(`error: ${err}`);
+          res.status(400).json({
+            error: err,
+          });
+        } else {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json({
+            users,
+          });
+        }
+      });
   });
 
   app.post("/users/admin", (req, res) => {
